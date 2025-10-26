4dad79152f071b4a649e085d70a7220a098860a0
Michael Eisenbraun <eisenbm@algonquincollege.com>
2025-10-26 17:30:46 -0400

    Creates starter files for part 3

--- app/Livewire/NoteViewer.php
+++ app/Livewire/NoteViewer.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Note;
 use Livewire\Attributes\Reactive;
 use Livewire\Component;
 
@@ -9,6 +10,14 @@ class NoteViewer extends Component
 {
     #[Reactive]
     public $selectedNote = [];
+
+    public function save () 
+    {
+        $note = Note::find($this->selectedNote['id']);
+        $note->title = $this->title;
+        $note->content = $this->content;
+        $note->save();
+    }
 
     public function render()
     {
