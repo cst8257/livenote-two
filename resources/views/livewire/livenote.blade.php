29cf1b19d70b48de2c9185bf25bdb9c29db2b5c2
Michael Eisenbraun <eisenbm@algonquincollege.com>
2025-10-27 11:45:47 -0400

    Creates starter files for part 4

--- resources/views/livewire/livenote.blade.php
+++ resources/views/livewire/livenote.blade.php
@@ -1,10 +1,13 @@
 <main class="container-fluid">
     <x-navbar />
     <div class="row vh-100 py-3">
-        <div class="col-12 col-md-5 col-lg-4 d-flex flex-column">
+        <div class="col-12 col-md-3 col-lg-2 d-flex flex-column">
+            <livewire:tag-list />
+        </div>
+        <div class="col-12 col-md-4 col-lg-4 d-flex flex-column">
             <livewire:note-list />
         </div>
-        <div class="col-12 col-md-7 col-lg-8 pt-5">
+        <div class="col-12 col-md-5 col-lg-6 pt-5">
             <livewire:note-viewer />
         </div>
     </div>
---
+++ resources/views/livewire/tag-list.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <div class="d-grid ps-3 py-3">
+        <a class="{{ !$selectedTag ? 'text-body fw-bold' : 'text-muted' }} py-2 pointer text-decoration-none mb-2">
+            All Notes
+        </a>
+        <h2 class="text-muted fs-6 mb-2">TAGS</h2>
+        <a class="text-muted py-2 ms-2 pointer text-decoration-none">Tag name</a>
+    </div>
+</div>
